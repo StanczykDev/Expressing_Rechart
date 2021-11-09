7e03ba3aa9a5af76c3d0c63b37e012d849aa9551
StanczykDev <stanczyk.dev@protonmail.com>
2021-11-09 19:26:49 +0300

    Repo initializing WIP.

---
+++ config/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const webpack = require("webpack");
+
+const ROOT_DIRECTORY = path.resolve(__dirname, '..');
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(ROOT_DIRECTORY, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: path.join(ROOT_DIRECTORY, "public/"),
+    port: 3000,
+    publicPath: "http://localhost:3000/dist/",
+    hotOnly: true
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+};
